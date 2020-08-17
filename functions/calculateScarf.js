b5e1160295d23cdae1675cc894343dfa82f68a8e
Renee Tremlab <rbalmert@gmail.com>
2020-08-17 11:59:19 -0700

    parse event info

--- functions/calculateScarf.js
+++ functions/calculateScarf.js
@@ -3,6 +3,6 @@ const querystring = require("querystring");
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: querystring.decode(event.body),
+    body: JSON.stringify(querystring.decode(event.body)),
   });
 };
